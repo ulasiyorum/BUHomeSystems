d28a885fcc69ef9f6dfcea37963dba40d75fbad5
Berkay Demir <berk3ay4@outlook.com>
2023-05-20 18:30:11 +0300

    advanced registration form coded as free_register

--- register-packages/advanced_register.php
+++ register-packages/advanced_register.php
@@ -5,8 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Advanced_Register</title>
+    
 </head>
 <body>
+    <div class="form-container p-2">
+
+   <form action="" method="post">
+   <div class="text-center">
+      <h1 class="mb-3 ">ADCANCED <span class="btn btn-primary opacity-75 pe-none">House Control</span></h1>
+      <h3>register now</h3>
+    </div>
+      
+      <?php
+         if(isset($error)){
+            foreach($error as $error){
+               echo '<span class="error-msg">'.$error.'</span>';
+            };
+         }
+         ?>
+         <input type="text" name="name" required placeholder="enter your name">
+         <input type="email" name="email" required placeholder="enter your email">
+         <input type="password" name="password" required placeholder="enter your password">
+         <input type="password" name="cpassword" required placeholder="confirm your password">
+         <select name="user_type">
+            <option value="consumer">consumer</option>
+            <option value="producer">producer</option>
+         </select>
+         <input type="submit" name="submit" value="register now" class="form-btn">
+         <p>already have an account? <a href="login_form.php">login now</a></p>
+      </form>
+
+      </div>
+
     
 </body>
 </html>
