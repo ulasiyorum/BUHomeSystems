b529bafeba651e06bcfb850969b9b0256e4bf347
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-05-20 16:17:29 +0300

    -

---
+++ src/lib/producer_main.js
@@ -0,0 +1,23 @@
+import Chart from "https://cdn.jsdelivr.net/npm/chart.js";
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3], // Example data
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
